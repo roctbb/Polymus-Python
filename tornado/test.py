7008b6b6da58f6c214fc7da8d32d153f3c6f83d6
roctbb <roctbb@gmail.com>
2017-02-14 19:20:43 +0300

    add tornado

--- tornado/test.py
+++ tornado/test.py
@@ -8,11 +8,18 @@ class MainHandler(tornado.web.RequestHandler):
 class HelloHandler(tornado.web.RequestHandler):
     def get(self):
         name = self.get_argument("name", "Анон")
-        self.write("Привет, {0}!".format(name))
+        self.write("<h2 style='align: center; color: blue;'>Привет, {0}!</h1>".format(name))
+
+class CatHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("cat.html")
 
 settings = [
     ('/', MainHandler),
     ('/hello', HelloHandler),
+    ('/cat', CatHandler),
+
+    ('/cats/(.*)', tornado.web.StaticFileHandler, {'path': 'cats'}),
 ]
 
 app = tornado.web.Application(settings)
